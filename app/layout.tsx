75e1d7497be4089d37845ddaa19515010d053761
555yash555 <63261431+555yash555@users.noreply.github.com>
2024-06-07 02:23:18 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,16 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Black Box Devspace",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Affordable Web & App Development | Black Box Devspace",
+  description: "Black Box Devspace specializes in creating professional websites and applications within budget. Expert web development and application services to help your business thrive online. Located in Croatia, we offer tailored solutions to meet your business needs.",
+  keywords: "affordable web development, budget web development, professional websites, application development, Black Box Devspace, web and app development, custom web applications, Croatia, web development services Croatia, app development services Croatia",
+  author: "Black Box Devspace",
+  "og:title": "Affordable Web & App Development | Black Box Devspace",
+  "og:description": "Expert web and application development services in Croatia. Black Box Devspace offers affordable and professional solutions to help your business thrive online.",
+  "og:type": "website",
+  "robots": "index, follow",
 };
+
 
 export default function RootLayout({
   children,
